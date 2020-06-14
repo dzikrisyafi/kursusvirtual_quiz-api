44238bea90a62678f49ab1abcbdef0343fbbea48
Dzikri Syafi <dzikriauliya@gmail.com>
2020-06-15 00:13:25 +0700

    adding marshaller

--- src/domain/quiz/quiz_marshaller.go
+++ src/domain/quiz/quiz_marshaller.go
@@ -1,1 +1,51 @@
 package quiz
+
+type PublicQuiz struct {
+	ID         int `json:"id"`
+	ActivityID int `json:"activity_id"`
+}
+
+type PublicQuizAndActivity struct {
+	ID         int `json:"id"`
+	ActivityID int `json:"activity_id"`
+}
+
+func (quizs Quizs) Marshall(isPublic bool) []interface{} {
+	result := make([]interface{}, len(quizs))
+	for index, quiz := range quizs {
+		result[index] = quiz.Marshall(isPublic)
+	}
+
+	return result
+}
+
+func (quiz Quiz) Marshall(isPublic bool) interface{} {
+	if isPublic {
+		return PublicQuiz{
+			ID:         quiz.ID,
+			ActivityID: quiz.ActivityID,
+		}
+	}
+
+	return quiz
+}
+
+func (quizs QuizsAndChoices) Marshall(isPublic bool) []interface{} {
+	result := make([]interface{}, len(quizs))
+	for index, quiz := range quizs {
+		result[index] = quiz.Marshall(isPublic)
+	}
+
+	return result
+}
+
+func (quiz QuizAndChoice) Marshall(isPublic bool) interface{} {
+	if isPublic {
+		return PublicQuizAndActivity{
+			ID:         quiz.ID,
+			ActivityID: quiz.ActivityID,
+		}
+	}
+
+	return quiz
+}
