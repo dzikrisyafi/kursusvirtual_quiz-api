3b08705c8fa99289f0fb4c1231b47c5fea4148b0
Dzikri Syafi <dzikriauliya@gmail.com>
2020-06-15 00:11:57 +0700

    adding marshaller

--- src/domain/choices/choices_marshaller.go
+++ src/domain/choices/choices_marshaller.go
@@ -1,1 +1,26 @@
 package choices
+
+type PublicChoice struct {
+	ID         int `json:"id"`
+	QuestionID int `json:"question_id"`
+}
+
+func (choices Choices) Marshall(isPublic bool) []interface{} {
+	result := make([]interface{}, len(choices))
+	for index, choice := range choices {
+		result[index] = choice.Marshall(isPublic)
+	}
+
+	return result
+}
+
+func (choice Choice) Marshall(isPublic bool) interface{} {
+	if isPublic {
+		return PublicChoice{
+			ID:         choice.ID,
+			QuestionID: choice.QuestionID,
+		}
+	}
+
+	return choice
+}
